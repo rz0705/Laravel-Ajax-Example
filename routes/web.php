aa01a8ee46458a6a3f2da23853ad348e6ebdabe5
Ronit z <ronitz@webmobtech.com>
2024-02-06 10:08:50 +0530

    Laravel AJAX example

---
+++ routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Http\Controllers\ProjectController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', [ProjectController::class, 'index']);
+
+Route::resource('projects', ProjectController::class);
